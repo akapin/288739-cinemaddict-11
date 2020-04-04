92991bacf00ca6c1793dca55984b5098a39b4ef6
Alex Kapin <alexander.kapin@outlook.com>
2020-04-04 11:28:56 +0300

    configure webpack; extract template functions into separate components

--- src/main.js
+++ src/main.js
@@ -1,101 +1,13 @@
-"use strict";
+import {createProfileTemplate} from "./components/profile.js";
+import {createMainNavigationTemplate} from "./components/main-navigation.js";
+import {createSortingTemplate} from "./components/sorting";
+import {createBoardTemplate} from "./components/board.js";
+import {createFilmTemplate} from "./components/film.js";
+import {createShowMoreButtonTemplate} from "./components/show-more-button.js";
+import {createFilmsAmountTemplate} from "./components/films-amount.js";
 
 const FILM_COUNT = 5;
 const FILM_EXTRA_COUNT = 2;
-
-const createProfileTemplate = () => {
-  return (
-    `<section class="header__profile profile">
-      <p class="profile__rating">Movie Buff</p>
-      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-    </section>`
-  );
-};
-
-const createMainNavigationTemplate = () => {
-  return (
-    `<nav class="main-navigation">
-      <div class="main-navigation__items">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-      </div>
-      <a href="#stats" class="main-navigation__additional">Stats</a>
-    </nav>`
-  );
-};
-
-const createSortTemplate = () => {
-  return (
-    `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
-    </ul>`
-  );
-};
-
-const createFilmsBoardTemplate = () => {
-  return (
-    `<section class="films">
-      <section class="films-list">
-        <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-
-        <div class="films-list__container">
-        </div>
-      </section>
-
-      <section class="films-list--extra">
-        <h2 class="films-list__title">Top rated</h2>
-
-        <div class="films-list__container">
-        </div>
-      </section>
-
-      <section class="films-list--extra">
-        <h2 class="films-list__title">Most commented</h2>
-
-        <div class="films-list__container">
-        </div>
-      </section>
-    </section>`
-  );
-};
-
-const createFilmCardTemplate = () => {
-  return (
-    `<article class="film-card">
-      <h3 class="film-card__title">The Dance of Life</h3>
-      <p class="film-card__rating">8.3</p>
-      <p class="film-card__info">
-        <span class="film-card__year">1929</span>
-        <span class="film-card__duration">1h 55m</span>
-        <span class="film-card__genre">Musical</span>
-      </p>
-      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
-      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
-      <a class="film-card__comments">5 comments</a>
-      <form class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
-      </form>
-    </article>`
-  );
-};
-
-const createShowMoreButtonTemplate = () => {
-  return (
-    `<button class="films-list__show-more">Show more</button>`
-  );
-};
-
-const createFilmsAmountTemplate = () => {
-  return (
-    `<p>130 291 movies inside</p>`
-  );
-};
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -107,22 +19,22 @@ const footerStatisticsElement = document.querySelector(`.footer__statistics`);
 
 render(siteHeaderElement, createProfileTemplate());
 render(siteMainElement, createMainNavigationTemplate());
-render(siteMainElement, createSortTemplate());
-render(siteMainElement, createFilmsBoardTemplate());
+render(siteMainElement, createSortingTemplate());
+render(siteMainElement, createBoardTemplate());
 
 const filmsListElement = siteMainElement.querySelector(`.films-list`);
 const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
 const filmsListExtraContainers = siteMainElement.querySelectorAll(`.films-list--extra .films-list__container`);
 
 for (let i = 0; i < FILM_COUNT; i++) {
-  render(filmsListContainerElement, createFilmCardTemplate());
+  render(filmsListContainerElement, createFilmTemplate());
 }
 
 render(filmsListElement, createShowMoreButtonTemplate());
 
 for (let i = 0; i < filmsListExtraContainers.length; i++) {
   for (let j = 0; j < FILM_EXTRA_COUNT; j++) {
-    render(filmsListExtraContainers[i], createFilmCardTemplate());
+    render(filmsListExtraContainers[i], createFilmTemplate());
   }
 }
 
