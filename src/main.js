fc81640e193d02e64de3269b7ce34a4a58ade7da
Alex Kapin <alexander.kapin@outlook.com>
2020-04-09 14:11:56 +0300

    render top rated and most commented blocks

--- src/main.js
+++ src/main.js
@@ -11,9 +11,10 @@ import {generateFilms} from "./mock/film.js";
 import {generateFilters} from "./mock/filter.js";
 
 const FILM_COUNT = 15;
-const FILM_EXTRA_COUNT = 2;
 const SHOWING_FILMS_COUNT_ON_START = 5;
 const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
+const SHOWING_TOP_RATED_FILMS_COUNT_ON_START = 2;
+const SHOWING_MOST_COMMENTED_FILMS_COUNT_ON_START = 2;
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -36,6 +37,8 @@ render(siteMainElement, createBoardTemplate());
 const filmsListElement = siteMainElement.querySelector(`.films-list`);
 const filmsListContainerElement = filmsListElement.querySelector(`.films-list__container`);
 const filmsListExtraContainers = siteMainElement.querySelectorAll(`.films-list--extra .films-list__container`);
+const topRatedFilmsList = filmsListExtraContainers[0];
+const mostCommentedFilmsList = filmsListExtraContainers[1];
 
 render(filmsListContainerElement, createFilmDetailsTemplate(films[0]));
 
@@ -46,11 +49,17 @@ films.slice(0, showingFilmsCount)
 
 render(filmsListElement, createShowMoreButtonTemplate());
 
-for (let i = 0; i < filmsListExtraContainers.length; i++) {
-  for (let j = 0; j < FILM_EXTRA_COUNT; j++) {
-    render(filmsListExtraContainers[i], createFilmTemplate(films[j]));
-  }
-}
+const showingTopRatedFilmsCount = SHOWING_TOP_RATED_FILMS_COUNT_ON_START;
+const filmsSortedByRating = films.slice().sort((a, b) => b.rating - a.rating);
+
+filmsSortedByRating.slice(0, showingTopRatedFilmsCount)
+  .forEach((film) => render(topRatedFilmsList, createFilmTemplate(film)));
+
+const showingMostCommentedFilmsCount = SHOWING_MOST_COMMENTED_FILMS_COUNT_ON_START;
+const filmsSortedByCommentsCount = films.slice().sort((a, b) => b.comments.length - a.comments.length);
+
+filmsSortedByCommentsCount.slice(0, showingMostCommentedFilmsCount)
+  .forEach((film) => render(mostCommentedFilmsList, createFilmTemplate(film)));
 
 render(footerStatisticsElement, createFilmsAmountTemplate(filmsAmount));
 
