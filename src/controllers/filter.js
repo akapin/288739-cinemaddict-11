fbbb1129db6ac5914dfda905c984319a0a6166c5
Alex Kapin <alexander.kapin@outlook.com>
2020-04-29 15:50:25 +0300

    7.1.3 Создаст контроллер для фильтрации

---
+++ src/controllers/filter.js
@@ -0,0 +1,50 @@
+import MainNavigationComponent from "../components/main-navigation.js";
+import {FilterType} from "../const.js";
+import {render, replace} from "../utils/render.js";
+import {getMoviesByFilter} from "../utils/filter.js";
+
+export default class FilterController {
+  constructor(container, moviesModel) {
+    this._container = container;
+    this._moviesModel = moviesModel;
+
+    this._activeFilterType = FilterType.ALL;
+    this._mainNavigationComponent = null;
+
+    this._onDataChange = this._onDataChange.bind(this);
+    this._onFilterChange = this._onFilterChange.bind(this);
+
+    this._moviesModel.setDataChangeHandler(this._onDataChange);
+  }
+
+  render() {
+    const container = this._container;
+    const allMovies = this._moviesModel.getMovies();
+    const filters = Object.values(FilterType).map((filterType) => {
+      return {
+        name: filterType,
+        count: getMoviesByFilter(allMovies, filterType).length,
+        checked: filterType === this._activeFilterType,
+      };
+    });
+    const oldComponent = this._mainNavigationComponent;
+
+    this._mainNavigationComponent = new MainNavigationComponent(filters);
+    this._mainNavigationComponent.setFilterChangeHandler(this._onFilterChange);
+
+    if (oldComponent) {
+      replace(this._mainNavigationComponent, oldComponent);
+    } else {
+      render(container, this._mainNavigationComponent);
+    }
+  }
+
+  _onFilterChange(filterType) {
+    this._moviesModel.setFilter(filterType);
+    this._activeFilterType = filterType;
+  }
+
+  _onDataChange() {
+    this.render();
+  }
+}
