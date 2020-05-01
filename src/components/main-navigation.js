35dd1b390b76e3beab5b4f29808b57a7a7929695
Alex Kapin <alexander.kapin@outlook.com>
2020-05-01 09:45:30 +0300

    7.1.8 Не отображать количество фильмов для фильтра All

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -9,7 +9,7 @@ const createFilterMarkup = (filter) => {
   const {name, count, active} = filter;
   return (
     `<a href="#${name}" class="main-navigation__item ${ active ? `main-navigation__item--active` : ``}">
-      ${capitalizeFirstLetter(name)} <span class="main-navigation__item-count">${count}</span>
+      ${capitalizeFirstLetter(name)} ${name === `all` ? `` : `<span class="main-navigation__item-count">${count}</span>`}
     </a>`
   );
 };
