5ff26d99cbce4b5a727e1c8657b209ebc5e48209
Alexander Kapin <alexander.kapin@gmail.com>
2020-05-03 09:31:18 +0300

    8.1.8 Реализует показ заглушки loading во время загрузки списка фильмов

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import PageController from "./controllers/page.js";
 import ProfileComponent from "./components/profile.js";
 import FilterController from "./controllers/filter.js";
 import SortController from "./controllers/sort.js";
+import LoadingComponent from "./components/loading.js";
 import MoviesAmountComponent from "./components/movies-amount.js";
 import MoviesModel from "./models/movies.js";
 import {getWatchedMovies} from "./utils/filter.js";
-import {render} from "./utils/render.js";
+import {render, remove} from "./utils/render.js";
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
@@ -21,15 +22,22 @@ filterController.render();
 const sortController = new SortController(siteMainElement, moviesModel);
 sortController.render();
 
-const pageController = new PageController(siteMainElement, moviesModel);
+const loadingComponent = new LoadingComponent();
+render(siteMainElement, loadingComponent);
 
 api.getMovies()
   .then((movies) => {
     moviesModel.setMovies(movies);
+  })
+  .finally(() => {
+    remove(loadingComponent);
+
+    const pageController = new PageController(siteMainElement, moviesModel);
+    pageController.render();
+
     const allMovies = moviesModel.getMoviesAll();
     const watchedMoviesCount = getWatchedMovies(allMovies).length;
 
-    pageController.render();
     render(siteHeaderElement, new ProfileComponent(watchedMoviesCount));
     render(footerStatisticsElement, new MoviesAmountComponent(allMovies.length));
   });
